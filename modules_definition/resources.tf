f3e469654888d1241ec020a6f83a3b3568e42a76
Your Name <you@example.com>
2024-08-04 01:01:41 +0200

    Add changes to mod file

--- modules_definition/resources.tf
+++ modules_definition/resources.tf
@@ -1,4 +1,4 @@
-resource "local_file" "local_files_mod" {
+resource "local_file" "local_files_m" {
     
     count = var.is_sensitive ? 0 : 1
 
@@ -7,12 +7,22 @@ resource "local_file" "local_files_mod" {
     file_permission = var.permission
 } 
 
-resource "local_sensitive_file" "local_file_sensitive_mod" {
+resource "local_sensitive_file" "local_file_sensitive_m" {
 
     count = var.is_sensitive ? 1 : 0
 
     filename = var.name
-    content = var.do_conversion ? var.file_content : base64encode(var.file_content) 
+    content = var.do_conversion ? base64encode(var.file_content) : var.file_content 
     file_permission = var.permission
     
+}
+
+moved {
+    from = local_file.local_files_mod
+    to = local_file.local_files_m
+}
+
+moved {
+    from = local_sensitive_file.local_file_sensitive_m
+    to = local_sensitive_file.local_file_sensitive_m
 }
