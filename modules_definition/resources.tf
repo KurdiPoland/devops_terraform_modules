a2c057b4743bfff07cb4b59bf4c82887e8989630
Your Name <you@example.com>
2024-08-04 00:21:18 +0200

    Add module definitions

---
+++ modules_definition/resources.tf
@@ -0,0 +1,18 @@
+resource "local_file" "local_files_mod" {
+    
+    count = var.is_sensitive ? 0 : 1
+
+    filename = var.name
+    content = var.do_conversion ? var.file_content : base64encode(var.file_content) 
+    file_permission = var.permission
+} 
+
+resource "local_file_sensitive" "local_file_sensitive_mod" {
+
+    count = var.is_sensitive ? 1 : 0
+
+    filename = var.name
+    content = var.do_conversion ? var.file_content : base64encode(var.file_content) 
+    file_permission = var.permission
+    
+}
---
+++ modules_definition/variables.tf
@@ -0,0 +1,19 @@
+variable name {
+    type = string
+    default = "file_name.txt"
+}
+
+variable file_content {
+    type = string
+    default = "Default contents"
+}
+
+variable is_sensitive {
+    type = bool
+    default = false
+}
+
+variable do_conversion {
+    type = bool
+    default = false
+}
